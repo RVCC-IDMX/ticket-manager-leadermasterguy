a769aec4e82f86db45737d6393b817e48a568ca5
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-10-23 04:58:34 -0400

    Finish event emitters

--- ticketManager.js
+++ ticketManager.js
@@ -4,9 +4,20 @@ const chalk = require('chalk');
 const { log } = console;
 
 class TicketManager extends EventEmitter {
- constructor(supply) {
-        super();
-        this.supply = supply;
+  constructor(supply) {
+    super();
+    this.supply = supply;
+  }
+
+  buy(email, price) {
+    if (this.supply > 0) {
+      this.supply -= 1;
+      this.emit('buy', email, price, Date.now());
+      return;
     }
+
+    this.emit('error', new Error('There are no more tickets left to purchase'));
+  }
 }
-}
+
+module.exports = TicketManager;
